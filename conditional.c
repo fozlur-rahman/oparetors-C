8a2a4de24a26aa166abed7147a0ccfcee60eaa83
unknown <fozlurrahman9933@gmail.com>
2023-05-07 04:30:45 +0600

    oparetors

---
+++ conditional.c
@@ -0,0 +1,61 @@
+// #include <stdio.h>
+// int main()
+// {
+
+//     int tk;
+//     scanf("%d", &tk);
+//     if (tk >= 100)
+//     {
+//         printf("ha");
+//     }
+//     else
+//     {
+//         printf("na");
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+
+//     int tk;
+//     scanf("%d", &tk);
+//     if (tk >= 5000)
+//     {
+//         printf("Go to Cox-bazar\n");
+//         if (tk >= 12000)
+//         {
+//             /* code */
+//             printf("Lets, Go to Saintmartin");
+//         }
+//         else
+//         {
+//             printf("Saintmartin r jawa holo na re vai");
+//         }
+//     }
+//     else
+//     {
+//         printf("nai");
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    int x = 0;
+    if (x == 1)
+    {
+        if (x >= 0)
+        {
+            printf("true ");
+        }
+        else
+        {
+            printf("false");
+        }
+    }
+}
---
+++ operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// int main()
+// {
+//     int a = 3;
+//     int b = 20;
+//     int sum = b % a;
+//     printf("%d", sum);
+
+//     return 0;
+// }
+
+int main()
+{
+    int a = 3;
+    int b = 20;
+    float sum = b * 1.0 / a;
+    printf("%0.2f", sum);
+
+    return 0;
+}
